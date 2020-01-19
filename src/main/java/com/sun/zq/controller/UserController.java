b40d9de63fb03dcf72f78f8e338654060a8cc522
sz48726 <sz48726@ly.com>
2020-01-19 11:08:47 +0800

    add log4j配置

--- src/main/java/com/sun/zq/controller/UserController.java
+++ src/main/java/com/sun/zq/controller/UserController.java
@@ -2,6 +2,9 @@ package com.sun.zq.controller;
 
 import com.sun.zq.model.User;
 import com.sun.zq.service.UserService;
+import net.sf.json.util.JSONUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private Logger log = LogManager.getLogger(this.getClass());
 
     @Autowired
     private UserService userService;
@@ -20,6 +24,7 @@ public class UserController {
     public String list(Model model) {
         List<User> userList =  userService.findAll();
         model.addAttribute("users", userList);
+        log.info("users = {}", JSONUtils.valueToString(userList));
         return "user";
     }
 }
--- src/main/java/com/sun/zq/listener/MyListener.java
+++ src/main/java/com/sun/zq/listener/MyListener.java
@@ -1,18 +1,25 @@
 package com.sun.zq.listener;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class MyListener implements ServletContextListener {
+    Logger log = LogManager.getLogger(this.getClass());
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        log.info("ServletContext 上下文初始化");
         System.out.println("ServletContext 上下文初始化");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        log.info("ServletContext 上下文销毁");
         System.out.println("ServletContext 上下文销毁");
     }
 }
